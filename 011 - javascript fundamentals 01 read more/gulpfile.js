034ac766983593f9b70fe24261f55790fe99c60b
Ethan Barrett <hello@buildingsareheavy.com>
2018-11-08 22:36:47 -0700

    refactored all the gulpfiles to the v4 syntax

--- 011 - javascript fundamentals 01 read more/gulpfile.js
+++ 011 - javascript fundamentals 01 read more/gulpfile.js
@@ -1,30 +1,35 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var log = require('fancy-log');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
+var log = require("fancy-log");
 
 // start 'sass'
-gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
-    .pipe(sass({includePaths: ['scss']}))
-    .on('error', log)
-    .pipe(gulp.dest('css'));
+gulp.task("sass", function() {
+  gulp
+    .src("scss/styles.scss")
+    .pipe(sass({ includePaths: ["scss"] }))
+    .on("error", log)
+    .pipe(gulp.dest("css"));
 });
 
 // start 'browser-sync'
-gulp.task('browser-sync', function () {
-    // watch index.html & all .css and .js files
-    browserSync.init(["**/*.html", "**/*.css", "**/*.js"], {
-        server: {
-            baseDire: "./",
-        },
-        // open in Firefox Developer Edition instead of default browser
-        browser: "Firefox Developer Edition"
-    });
+gulp.task("browser-sync", function() {
+  // watch index.html & all .css and .js files
+  browserSync.init(["**/*.html", "**/*.css", "**/*.js"], {
+    server: {
+      baseDire: "./"
+    },
+    // open in Firefox Developer Edition instead of default browser
+    browser: "Firefox Developer Edition"
+  });
 });
 
 // combine 'sass' and 'browser-sync'
 // just run 'gulp'
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-});
+gulp.task(
+  "default",
+  // added gulp.series and gulp.parallel becuase of Gulp v4
+  gulp.series(gulp.parallel("sass", "browser-sync"), function() {
+    gulp.watch("scss/*.scss", ["sass"]);
+  })
+);
