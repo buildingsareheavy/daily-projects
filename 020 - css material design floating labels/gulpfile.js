10dc358cd19771521cc955590a3734064c0377f2
Ethan Barrett <apartamento@Ethans-MacBook-Pro.local>
2019-11-03 15:20:19 -0700

    changed gulp.js and added mobile CSS to 020

--- 020 - css material design floating labels/gulpfile.js
+++ 020 - css material design floating labels/gulpfile.js
@@ -1,39 +1,38 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync");
-const log = require("fancy-log");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const log = require('fancy-log');
 
 // start 'scss'
 function scss() {
   return gulp
-    .src("./scss/styles.scss")
-    .pipe(sass({ includePaths: ["scss"] }))
-    .on("error", log)
-    .pipe(gulp.dest("./css"));
+    .src('./scss/styles.scss')
+    .pipe(sass({ includePaths: ['scss'] }))
+    .on('error', log)
+    .pipe(gulp.dest('./css'));
 }
 
 // start 'bs'
 function bs() {
   // watch index.html & all .css and .js files
-  return browserSync.init(["**/*.html", "**/*.css", "**/*.js"], {
+  return browserSync.init(['**/*.html', '**/*.css', '**/*.js'], {
     server: {
-      baseDire: "./"
+      baseDire: './',
     },
-    browser: "Firefox Developer Edition"
   });
 }
 
 // watch for changes to .scss files and rerun 'scss'
-const watchAll = gulp.watch("./scss/*.scss", gulp.series(scss));
+const watchAll = gulp.watch('./scss/*.scss', gulp.series(scss));
 
 // start 'gulp'
 gulp.task(
-  "default",
+  'default',
   // combines 'scss' and 'bs'
   gulp.series(gulp.parallel(bs, scss), function() {
     // Not sure why this needs to be declared as a variable and can't just be inside this function?
     return watchAll;
-  })
+  }),
 );
